0c87839c0655aadb2cde2e3ee603ca75c04ee5eb
FrostBlade001 <udov2008@gmail.com>
2020-11-30 12:59:36 +0500

    Refactor textprocessor_test a little bit

--- task3/pkg/textprocessor/textprocessor_test.go
+++ task3/pkg/textprocessor/textprocessor_test.go
@@ -1,71 +1,62 @@
 package textprocessor
 
 import (
-	"io"
 	"os"
 	"strings"
 	"testing"
 )
 
 type TestCase struct {
-	text           io.Reader
+	text           string
 	wordsLength    int
 	expectedWords  []string
 	expectedCounts []int
 }
 
 func TestCountWords(t *testing.T) {
-	f, _ := os.Open("testdata/text.txt")
-	defer f.Close()
 	testCases := []TestCase{
 		{
-			strings.NewReader(""),
+			"",
 			3,
 			[]string{"", "", "", "", "", "", "", "", "", ""},
 			[]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		},
 		{
-			strings.NewReader(`
+			`
 				Some words! in large string. Maybe it's not so large!! as you think
 				Some words in large string. Maybe not so -large as you think.
 				Some words? in large string. Maybe not so ?large as you think?
-			`),
+			`,
 			3,
 			[]string{"words", "large", "", "", "", "", "", "", "", ""},
 			[]int{3, 6, 0, 0, 0, 0, 0, 0, 0, 0},
 		},
 		{
-			strings.NewReader(`
+			`
 				Hello hello again! Gree244tings my friend. Hello again I told you that.
 				Words. that. not. be. in. this. list. cheers cheers! Happy hell yeah!
 				Repeat repeat inlistedword repeat repeat repeat repeat again and repeat repeat!!
 				And this 'repeat' word won't be on our list. But golang golang will will be in
-			`),
+			`,
 			3,
 			[]string{"again", "greetings", "told", "happy", "hell", "inlistedword", "word", "wont", "golang", "will"},
 			[]int{3, 1, 1, 1, 1, 1, 1, 1, 2, 2},
 		},
 		{
-			strings.NewReader(`
+			`
 				Hello hello again! Greetings my friend. Hello again I told you that.
 				Words. that. not. be. in. this. list. cheers cheers! Happy hell yeah!
 				Repeat repeat inlistedword repeat repeat repeat repeat again and repeat repeat!!
 				And this 'repeat' word won't be on our list. But golang golang will will be in
-			`),
+			`,
 			5,
 			[]string{"greetings", "inlistedword", "golang", "", "", "", "", "", "", ""},
 			[]int{1, 1, 2, 0, 0, 0, 0, 0, 0, 0},
 		},
-		{
-			f,
-			3,
-			[]string{"like", "told", "looked", "marry", "went", "love", "want", "into", "took", "cant"},
-			[]int{35, 29, 39, 21, 62, 32, 29, 29, 20, 18},
-		},
 	}
 
 	for _, tt := range testCases {
-		res := New(tt.text, tt.wordsLength).CountWords()
+		res := New(strings.NewReader(tt.text), tt.wordsLength).CountWords()
 		for i, v := range res.GetTop(10) {
 			if tt.expectedWords[i] != v {
 				t.Fatalf("unexpected word at index %d: wont %s - got %s", i, tt.expectedWords[i], v)
@@ -73,6 +64,23 @@ func TestCountWords(t *testing.T) {
 			if tt.expectedCounts[i] != res.GetCount(v) {
 				t.Fatalf("unexpected count for '%s': wont %d - got %d", v, tt.expectedCounts[i], res.GetCount(v))
 			}
+		}
+	}
+}
+
+func TestCountWordsOnFile(t *testing.T) {
+	expectedWords := []string{"like", "told", "looked", "marry", "went", "love", "want", "into", "took", "cant"}
+	expectedCount := []int{35, 29, 39, 21, 62, 32, 29, 29, 20, 18}
+	f, _ := os.Open("testdata/text.txt")
+	defer f.Close()
+
+	res := New(f, 3).CountWords()
+	for i, v := range res.GetTop(10) {
+		if expectedWords[i] != v {
+			t.Fatalf("unexpected word at index %d: wont %s - got %s", i, expectedWords[i], v)
+		}
+		if expectedCount[i] != res.GetCount(v) {
+			t.Fatalf("unexpected count for '%s': wont %d - got %d", v, expectedCount[i], res.GetCount(v))
 		}
 	}
 }
